14aa6666ceafd3285b4b323ad92d80b31fe6aaeb
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-02-24 17:24:58 +0300

    CheckIndentifier.php

--- LW3/CheckIndentifier.php
+++ LW3/CheckIndentifier.php
@@ -1,10 +1,10 @@
 <?php
-    if(empty($_GET['text']))
-        exit('Ошибка - пустой вход');
-    $text = $_GET["text"];
-    preg_match("/^[a-zA-Z][a-zA-Z0-9]+/", $text, $matches);//проверяем, входят ли в строку только допускаемые символы matches = правильной строке
-    if(count($matches) <= 1)//если первый символ неправильный()
-        exit("Error - first char not alphabet char");
-    /*if(!ctype_alpha(substr($text, 0)))
-        exit('Ошибка - первый симбол цифра');*/
-    echo ('Успешно');
+    if(empty($_GET['text'])) //проверяем, не пустой ли вход
+        exit('Error - empty input');
+    $text = $_GET['text'];
+    preg_match('/^[a-zA-Z][a-zA-Z0-9]+/', $text, $matches);//проверяем, входят ли в строку только допускаемые символы matches = правильной строке
+    $length_math = strlen($matches[0]); //вычисляем длину строки matches
+    $different = strlen($text) - $length_math;  //разность длины входной строки и длины строки matches
+    if($different > 0)  //если строки не совпадают по длине, то ошибка
+        exit('Error - char in ' . $length_math . ' position not bet or digit');
+    echo 'Success';
