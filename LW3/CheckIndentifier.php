6ccce2a365a55c25980f0ab228e820f893c3c8d0
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-02-24 10:59:28 +0300

    CheckIndentifier.php

--- LW3/CheckIndentifier.php
+++ LW3/CheckIndentifier.php
@@ -1,12 +1,12 @@
 <?php
-    if(empty($_GET["text"]))
-        exit("Ошибка - пустой вход");
+    if(empty($_GET['text']))
+        exit('Ошибка - пустой вход');
     $text = $_GET["text"];
-    preg_match("/^[a-zA-Z][a-zA-Z0-9]+/", $text, $matches);
+    preg_match("/^[a-zA-Z][a-zA-Z0-9]+/", $text, $matches); //проверяем, входят ли в строку только допускаемые символы matches = правильной строке
     if(count($matches) <= 1)
-        exit("Ошибка - первый симбол не буква");
+        exit('Ошибка - первый симбол не буква');
     $length_math = strlen($matches[0]);
     $different = strlen($text) - $length_math;
     if($different > 0)
-        exit("Ошибка - символ в " . $length_math . "позиции не цифра");
-    echo "Успешно";
+        exit('Ошибка - символ в ' . $length_math . 'позиции не цифра');
+    echo ('Успешно');
