8b339a7f96a4d801d341cc843878ad9f683c29dd
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-02-24 11:21:20 +0300

    web_dev

--- LW3/CheckIndentifier.php
+++ LW3/CheckIndentifier.php
@@ -2,11 +2,9 @@
     if(empty($_GET['text']))
         exit('Ошибка - пустой вход');
     $text = $_GET["text"];
-    preg_match("/^[a-zA-Z][a-zA-Z0-9]+/", $text, $matches); //проверяем, входят ли в строку только допускаемые символы matches = правильной строке
-    if(count($matches) <= 1)
-        exit('Ошибка - первый симбол не буква');
-    $length_math = strlen($matches[0]);
-    $different = strlen($text) - $length_math;
-    if($different > 0)
-        exit('Ошибка - символ в ' . $length_math . 'позиции не цифра');
+    preg_match("/^[a-zA-Z][a-zA-Z0-9]+/", $text, $matches);//проверяем, входят ли в строку только допускаемые символы matches = правильной строке
+    if(count($matches) <= 1)//если первый символ неправильный()
+        exit("Error - first char not alphabet char");
+    /*if(!ctype_alpha(substr($text, 0)))
+        exit('Ошибка - первый симбол цифра');*/
     echo ('Успешно');
--- LW3/RemoveExtraBlanks.php
+++ LW3/RemoveExtraBlanks.php
@@ -1,5 +1,5 @@
 <?php
     header("Content-Type: text/plain");
     $text = $_GET["text"];
-    $text = preg_replace('/\s+/', ' ', $text);
+    $text = preg_replace('/\s+/', ' ', $text); //Заменяется непрерывная строка пробелов на 1 пробел
     echo trim($text);
