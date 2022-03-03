b38fb68fe3afa1694d0635323948b9d3af162fe7
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-03-03 17:34:45 +0300

    Debug

--- LW3/SurveySaver.php
+++ LW3/SurveySaver.php
@@ -1,28 +1,28 @@
 <?php
     header("Content-Type: text/plain");
-    const DATA_PATH = "answer.txt";
+    const DATA_PATH = 'answer.txt';
     $text = $_GET["text"];
     $fields_to_bind = [
         'first_name',
         'last_name',
         'email',
-        'age'
+        'age',
     ];
-    $required_fields = [
-        'email'
-    ];
+    $required_fields = ['email'];
     $dataset = [];
 
     foreach ($required_fields as $value)
         if(empty($_GET[$value]))
             exit('Error of input');
-
-    foreach ($fields_to_bind as $field)
-        if(isset($_GET[$field]))
-            $dataset[$field] = $_GET[$field];
+    $i = 0;
+    foreach ($fields_to_bind as $field) {
+        if (isset($_GET[$field]))
+            $dataset[$field] = $fields_to_bind[$i] . ": " . $_GET[$field] . "\n";
+        $i++;
+    }
 
     file_put_contents(
         DATA_PATH,
-        implode("\n", $dataset),
+        $dataset,
     );
     echo('Response generated');
