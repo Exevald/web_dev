8fe8455d9f98aae9c8b5b1a9d6ed7a2ec47b2db8
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-04-01 19:40:12 +0300

    Work on lw6

--- LW3/SurveySaver.php
+++ LW3/SurveySaver.php
@@ -1,6 +1,5 @@
 <?php
     header("Content-Type: text/html");
-    const DATA_PATH = 'email.txt';
     $text = $_GET["text"];
     $fields_to_bind = ['first_name', 'last_name', 'email', 'age',];
     $required_fields = ['email'];
@@ -10,10 +9,11 @@
         if(empty($_GET[$value]))
             exit('Error of input. In request must be an email');
     $i = 0;
-//    foreach ($fields_to_bind as $field) {
-//        if ((isset($_GET[$field]) && (is_writable(DATA_PATH))))
-//            $dataset[$field] = $fields_to_bind[$i] . ": " . $_GET[$field] . "\n";
-//        $i++;
-//    }
+    foreach ($fields_to_bind as $field) {
+        if ((isset($_GET[$field])))
+            $dataset[$field] = $fields_to_bind[$i] . ": " . $_GET[$field] . "\n";
+        $i++;
+    }
 //    file_put_contents(DATA_PATH, $dataset);
-//    echo('Response generated');
+    file_put_contents($_GET['email'] . '.txt', $dataset);
+    echo('Response generated');
