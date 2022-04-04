ae94bae9cf96dd1dbf14f8034df8d4e1ac848743
Konstantin <64555204+Exevald@users.noreply.github.com>
2022-04-04 16:42:00 +0300

    Work on lw6

--- LW3/SurveySaver.php
+++ LW3/SurveySaver.php
@@ -5,15 +5,18 @@
     $fields_to_bind = ['first_name', 'last_name', 'email', 'age',];
     $required_fields = ['email'];
     $dataset = [];
+    fopen($filename, 'r+');
 
     foreach ($required_fields as $value)
         if(empty($_GET[$value]))
             exit('Error of input. In request must be an email');
     $i = 0;
     foreach ($fields_to_bind as $field) {
-        if (isset($_GET[$field]))
+        if ((isset($_GET[$field])) && (!strpos(file_get_contents($filename), $fields_to_bind[$i]))) {
             $dataset[$field] = $fields_to_bind[$i] . ": " . $_GET[$field] . "\n";
+            echo($dataset[$field] . ' ');
+        }
+        file_put_contents($filename, $dataset);
         $i++;
     }
-    file_put_contents($filename, $dataset);
     echo('Response generated');
