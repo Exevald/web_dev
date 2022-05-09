b6366d7d0a69d9428255aa1b400a7b8265769866
Exevald <kalinin.konst.official@gmail.com>
2022-05-09 19:40:02 +0300

    work popUp

--- LW10/js/popup.js
+++ LW10/js/popup.js
@@ -40,47 +40,31 @@ function main() {
     function createPopup() {
         popUp.classList.add('popUp');
         popUp.innerHTML =
-        '<div class="form" id="pop-up">\n' +
-        '    <div class="form-crossbar">\n' +
-        '        <img class="form-crossbar__image"\n' +
-        '            alt="exit"\n' +
-        '            src="../images/form-exit.png"/>\n' +
-        '    </div>\n' +
-        '        <img class="form__image"\n' +
-        '             src="../images/welcome.png"\n' +
-        '             alt="welcome"/>\n' +
-        '        <div class="form-header">\n' +
-        '            <h3 class="form-header__text">Записаться на курс</h3>\n' +
-        '        </div>\n' +
-        '        <div class="form-data">\n' +
-        '            <form class="form-data" method="POST" action="register.php">\n' +
-        '                <label class="form-label">\n' +
-        '                    <input class="form-label form-label__text" type="text" name="name" placeholder="Ваше имя"/>\n' +
-        '                </label>\n' +
-        '                <label class="form-label">\n' +
-        '                    <input class="form-label form-label__text" type="email" name="email" placeholder="Email"/>\n' +
-        '                </label>\n' +
-        '                <label class="form-label">\n' +
-        '                    <select name="{%activity%}" class="form-select form-label__text">\n' +
-        '                        <option style="display: none" selected disabled>Деятельность</option>\n' +
-        '                        <option value="programmer">Программист</option>\n' +
-        '                        <option value="designer">Дизайнер</option>\n' +
-        '                        <option value="marketer">Маркетолог</option>\n' +
-        '                    </select>\n' +
-        '                </label>\n' +
-        '                <div class="form-checkbox">\n' +
-        '                    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" class="subscribe-block"/>\n' +
-        '                    <label for="subscribeNews" class="form-checkbox__text">\n' +
-        '                        Согласен получать информационные материалы о старте курса\n' +
-        '                    </label>\n' +
-        '                </div>\n' +
-        '                <p>\n' +
-        '                    <input class="submit-block" type="submit" value="Записаться на курс"/>\n' +
-        '                </p>\n' +
-        '            </form>\n' +
-        '        </div>\n' +
-        '    </div>';
-
+        `<div class="form-wrap">
+            <img class="image-welcome" src="images/welcome.png" alt="welcome">
+            <h2 class="form-text">Записаться на курс</h2>
+            <form class="form-section" action="register.php" method="POST">
+                <label>
+                    <input class="input-wrap" type="text" name="name" placeholder="Ваше имя"/>
+                </label>
+                <label>
+                    <input class="input-wrap" type="text" name="email" placeholder="Email"/>
+                </label>
+                    <select name="%activity%" class="input-wrap">
+                        <option selected disabled>Деятельность</option>
+                        <option value="programmer">Программист</option>
+                        <option value="designer">Дизайнер</option>
+                        <option value="marketer">Маркетолог</option>
+                    </select>
+                <div class="checkbox-block-wrap">
+                    <input type="checkbox" id="subscribeNews" name="subscribe" value="newsletter" class="subscribe-block">
+                    <label for="subscribeNews" class="checkbox-text">Согласен получать информационные материалы о старте курса</label>
+                </div>
+                <p>
+                    <input type="submit" class="submit-block" value="Записаться на курс"/>
+                </p>
+            </form>
+        </div>`;
         return popUp
     }
 
